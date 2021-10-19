74af822309f5bad81fa2a1de467b8c3ead00eb44
ismaely <7ilipe@gmail.com>
2021-10-19 12:52:28 +0100

    change static root

--- academy/settings.py
+++ academy/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (BASE_DIR, 'static')
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATICFILES_DIRS = (BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_URL = '/media/'
