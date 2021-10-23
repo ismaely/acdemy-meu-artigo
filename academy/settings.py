ca3cdd9cf47cc8bfb02ed56c4b8572a5b74c33f3
Gunza Ismael <7ilipe@gmail.com>
2021-10-23 09:23:59 +0100

    static

--- academy/settings.py
+++ academy/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 STATICFILES_DIRS = (BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
