e001739a45f26c9fd0ba37a72d75f7c99911d2b0
ismaely <7ilipe@gmail.com>
2021-10-23 22:08:23 +0100

    new

--- academy/settings.py
+++ academy/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 
 #STATICFILES_DIRS = (BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
