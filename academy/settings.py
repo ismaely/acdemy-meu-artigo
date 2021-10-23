f6a18ea7fd9f62e0777a5bdce7ad8672d85b0ba8
ismaely <7ilipe@gmail.com>
2021-10-24 00:37:00 +0100

    base new

--- academy/settings.py
+++ academy/settings.py
@@ -129,10 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR, 'static/'
+STATIC_ROOT = '{}/static'.format(BASE_DIR)
 
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
