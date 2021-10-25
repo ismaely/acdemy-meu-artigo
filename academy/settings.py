b5bb1a2721739263c357f2300a9f6c26eb96ba46
Gunza Ismael <7ilipe@gmail.com>
2021-10-25 11:48:45 +0100

    fixe

--- academy/settings.py
+++ academy/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 else:
     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
