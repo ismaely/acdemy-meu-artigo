663a4effa251bb3e9c103cf9f2d010dec34b7c24
ismaely <7ilipe@gmail.com>
2021-10-25 08:58:08 +0100

    home change

--- academy/settings.py
+++ academy/settings.py
@@ -128,9 +128,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#SATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+
+SATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
 
