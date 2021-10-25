55466cab07da9ee11c7b1a47bd9c3b41b7791456
Gunza Ismael <7ilipe@gmail.com>
2021-10-25 12:15:38 +0100

    static files

--- academy/settings.py
+++ academy/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 else:
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
 
 SATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
