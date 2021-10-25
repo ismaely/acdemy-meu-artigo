7f1cd4d8c6d94cbf471629e8f86f26ed812d34bd
ismaely <7ilipe@gmail.com>
2021-10-25 12:21:55 +0100

    change fi

--- academy/settings.py
+++ academy/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-18&vp!u1q#wumau3mv&ivi2(8dfba=kz%ifo2_m-vw@02mb3+i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'imbondeiro.herokuapp.com']
 
