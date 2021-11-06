1721f044b7846588c198f21a5735e9655bf1d7e4
ismaely <7ilipe@gmail.com>
2021-11-06 11:27:01 +0100

    change file base

--- instituicao/models.py
+++ instituicao/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Instituicao(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=60, blank=True, null=True, default=" ")
+    localizacao = models.CharField(max_length=60, blank=True, null=True, default=" ")
+    sigla = models.CharField(max_length=60, blank=True, null=True, default=" ")
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    logo = models.ImageField(upload_to='logo/%Y/', blank=True, null=True, default="dip.jpg")
+
+    def __str__(self):
+        return "%s" % (self.nome)
