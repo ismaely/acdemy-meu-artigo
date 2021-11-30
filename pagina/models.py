6c0fd916857d45fa0c936dfccac3a1081ff3a59c
ismaely <7ilipe@gmail.com>
2021-11-30 11:53:50 +0100

    change files

--- pagina/models.py
+++ pagina/models.py
@@ -7,18 +7,18 @@ class Documenmto(models.Model):
     instituicao = models.CharField(max_length=220, blank=True, null=True,default="")
     ano = models.CharField(max_length=4, blank=True, null=True, default="")
     autor = models.CharField(max_length=400, blank=True, null=True, default="")
-    resumo = models.CharField(max_length=500, blank=True, null=True, default="")
+    resumo = models.TextField(max_length=500, blank=True, null=True, default="")
     palavras_chaves = models.CharField(max_length=350, blank=True, null=True, default="")
     categoria = models.CharField(max_length=120, choices=CATEGORIA)
     tipo = models.CharField(max_length=100, choices=TIPO)
     editora = models.CharField(max_length=300, blank=True, null=True, default="")
     orientador = models.CharField(max_length=500, blank=True, null=True, default="")
-    uri = models.CharField(max_length=300, blank=True, null=True, default="")
+    uri = models.URLField(max_length=300, blank=True, null=True, default="")
     tipo_acesso = models.CharField(max_length=500, blank=True, null=True, default="")
     data_publicacao = models.CharField(max_length=20)
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now_add=True)
-    arquivo = models.FileField(upload_to='arquivos/%Y/', blank=True, null=True)
+    arquivo = models.FileField(upload_to='uploads/%Y/', blank=True, null=True)
 
     def __str__ (self):
         return self.id
