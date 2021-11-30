ae934609dadbb5d1daa367d6515f71bb31eba987
ismaely <7ilipe@gmail.com>
2021-11-30 11:22:21 +0100

    new model and files

---
+++ core/opcoes.py
@@ -0,0 +1,15 @@
+
+CATEGORIA = (
+    ('Saúde', 'Saúde'),
+    ('Ciência', 'Ciência'),
+    ('Biológia', 'Biológia'),
+    ('Cultura', 'Cultura'),
+)
+
+TIPO = (
+    ('Artigo', 'Artigo'),
+    ('Livro', 'Livro'),
+    ('Palestra', 'Palestra'),
+    ('Monografia', 'Monografia'),
+    ('Dissertação', 'Dissertação'),
+)
--- pagina/models.py
+++ pagina/models.py
@@ -1,11 +1,20 @@
 from django.db import models
-
+from core.opcoes import CATEGORIA, TIPO
 # Create your models here.
 
 class Documenmto(models.Model):
-    titulo = models.CharField(max_length=120, blank=True, null=True,default="")
-    instituicao = models.CharField(max_length=120, blank=True, null=True,default="")
+    titulo = models.CharField(max_length=520, blank=True, null=True,default="")
+    instituicao = models.CharField(max_length=220, blank=True, null=True,default="")
     ano = models.CharField(max_length=4, blank=True, null=True, default="")
+    autor = models.CharField(max_length=400, blank=True, null=True, default="")
+    resumo = models.CharField(max_length=500, blank=True, null=True, default="")
+    palavras_chaves = models.CharField(max_length=350, blank=True, null=True, default="")
+    categoria = models.CharField(max_length=120, choices=CATEGORIA)
+    tipo = models.CharField(max_length=100, choices=TIPO)
+    editora = models.CharField(max_length=300, blank=True, null=True, default="")
+    orientador = models.CharField(max_length=500, blank=True, null=True, default="")
+    uri = models.CharField(max_length=300, blank=True, null=True, default="")
+    tipo_acesso = models.CharField(max_length=500, blank=True, null=True, default="")
     data_publicacao = models.CharField(max_length=20)
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now_add=True)
